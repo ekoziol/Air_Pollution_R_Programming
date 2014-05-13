a3e48d2463966006657e75601900e2adfd4efabb
Eric Koziol <e.trip.k@gmail.com>
2014-05-12 19:43:07 -0500

    complete corr.R

--- corr.R
+++ corr.R
@@ -1,3 +1,20 @@
-corr <- function(directory, threshold){
+corr <- function(directory, threshold=0){
+  if (!file.exists(directory)){  
+    return("Error: Directory does not exist!")
+  }
   
+  fileList <- paste(directory, "/", list.files(directory), sep="")
+  correlations <- c()
+  for(f in fileList){
+    tempFrame <- read.csv(f)
+    completeCases <-  complete.cases(tempFrame)
+    #print(completeCases)
+    numberOfCases <- sum(completeCases)
+    #print(tempFrame[completeCases,"nitrate"])
+    if(numberOfCases > threshold){
+      t <- tempFrame[completeCases,]
+      correlations <- append(correlations, cor(t$sulfate, t$nitrate))
+    }
+  }
+  return(correlations)
 }
